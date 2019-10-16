5205de031b45e1d6288d66d5d579078506b2d73e
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2019-10-16 10:07:58 +0530

    updated
    Signed-off-by: Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>

--- SHODAN_USAGE/REST_API_BASED/01_ip_search.py
+++ SHODAN_USAGE/REST_API_BASED/01_ip_search.py
@@ -14,7 +14,6 @@ class Shodan:
     def __init__(self):
         self.base_url = 'https://api.shodan.io'
         self.YOUR_API_KEY = os.getenv('SHODAN')
-        self.vulnerable_ips = ''
 
     def get_host_info_by_ip(self, ip, minify=True, history=False):
         """
@@ -24,22 +23,20 @@ class Shodan:
         try:
             response = requests.get(URL).json()
             if (not response.get('error')) and response.get('vulns'):
-                self.vulnerable_ips += ip + '\n'
+                return response['ip_str']
                 # pprint(response)
-                print(response['ip_str'])#, response['ports'], response.get('vulns'), '\n\n')
+                # print(response['ip_str'], response['ports'], response.get('vulns'), '\n\n')
         except requests.exceptions.SSLError:
             time.sleep(10)
 
     def filter_vulnerable_ips(self, file_name):
+        g = open('vulnerable_ips.txt', 'a')
         with open('ip_addresses.txt', 'r') as f:
             for ech_ip in set(f.readlines()):
-                s.get_host_info_by_ip(ech_ip.strip())
-
-        if self.vulnerable_ips:
-            print(self.vulnerable_ips)
-            with open('vulnerable_ips.txt', 'w') as g:
-                g.write(self.vulnerable_ips)
-                g.close()
+                vul_ip = s.get_host_info_by_ip(ech_ip.strip())
+                if vul_ip:
+                    g.write(vul_ip + '\n')
+        g.close()
 
 s = Shodan()
 s.filter_vulnerable_ips('ip_addresses.txt')
